43bc80a79a0e09465adba467e77591e23435d5bf
oliver-sunwc <oliver.sunwc@gmail.com>
2019-01-25 18:07:27 -0800

    as;ldkfj

--- ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/spineTest.java
+++ ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/spineTest.java
@@ -16,6 +16,7 @@ import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.SensorColor;
@@ -42,15 +43,68 @@ import java.util.Locale;
 @TeleOp(name = "spineTest")
 
 public class spineTest extends OpMode {
-    DcMotor spine;
+    roverHMAP robot = new roverHMAP();
+    double rx,ry,lx;
+    double hang;
+    double hangPow = 1;
+
+    boolean fliptog = false;
+    boolean f;
+    boolean rotatetog = false;
+    boolean r;
+    boolean ninja = false;
+    boolean n;
+    int intakeTog = 0;
+    boolean i;
+    int flipAng = 2500;
+    boolean inNinja = false;
+    boolean iN;
+    boolean flapTog = false;
+    boolean fL;
+
+    int initialPosition;
+    ElapsedTime stallTime = new ElapsedTime();
 
     @Override
     public void init(){
-        spine = hardwareMap.get(DcMotor.class, "s");
+        robot.init(hardwareMap);
+        robot.inFlip.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.inFlip.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        robot.hang.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.hang.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        initialPosition = robot.hang.getCurrentPosition();
     }
 
     @Override
     public void loop(){
-        spine.setPower(gamepad1.left_stick_y);
+        if(!f && gamepad1.x) {
+            if(!fliptog) {
+                robot.flipLArm.setPosition(1);
+                robot.flipRArm.setPosition(0);
+                robot.rotateArm.setPosition(0.50);
+                fliptog = true;
+            } else {
+                robot.flipLArm.setPosition(0);
+                robot.flipRArm.setPosition(1);
+                robot.rotateArm.setPosition(0);
+                fliptog = false;
+            }
+        }
+        f = gamepad1.x;
+
+        if(!r && gamepad1.y) {
+            if(!rotatetog) {
+                rotatetog = true;
+                robot.flipLArm.setPosition(0.7);
+                robot.flipRArm.setPosition(0.3);
+            } else {
+                robot.flipLArm.setPosition(1);
+                robot.flipRArm.setPosition(0);
+                rotatetog = false;
+            }
+        }
+
+        r = gamepad1.y;
     }
 }
